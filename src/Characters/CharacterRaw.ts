f3abc68002de1f59058fb69c02637ce20bf97955
Яков Панов <diegoling33@yandex.ru>
2020-11-16 17:10:32 +0300

    Added new characters raw types

---
+++ src/Characters/CharacterRaw.ts
@@ -0,0 +1,71 @@
+
+export interface CharacterCoreRaw{
+    wow_id: number;
+    name: string;
+}
+
+
+export type CharacterEquipmentType = "HEAD" | "NECK" | "SHOULDER" | "CHEST" | "WAIST" | "LEGS" | "FEET"
+    | "WRIST" | "HANDS" | "FINGER_1" | "FINGER_2" | "TRINKET_1" | "TRINKET_2"
+    | "BACK" | "MAIN_HAND" | "OFF_HAND" | "TABARD";
+
+export interface CharacterEquipmentRaw {
+    wow_id: number;
+    character_id: number;
+    image_id: number | null;
+
+    title: string;
+
+    slot: CharacterEquipmentType;
+    inventory_type: string;
+    level: number;
+
+    quantity: number;
+    quality: number;
+
+    item_class_id: number;
+    item_subclass_id: number;
+
+    stats?: string;
+}
+
+export interface CharacterRoleRaw {
+    role_index: number;
+    title: string;
+}
+
+export interface CharacterRaceRaw {
+    wow_id: number;
+    title: string;
+}
+
+export interface CharacterClassRaw {
+    wow_id: number;
+    title: string;
+}
+
+export interface CharacterActiveSpecRaw {
+    wow_id: number;
+    title: string;
+    type: number;
+}
+
+export interface CharacterRaw extends CharacterCoreRaw{
+    gender: string;
+    level: number;
+    faction: string;
+    gear: number;
+
+    guild_role: number;
+    meta_text: string;
+
+    role: CharacterRoleRaw;
+    character_race: CharacterRaceRaw;
+    character_class: CharacterClassRaw;
+    character_spec: CharacterClassRaw;
+
+    realm_id: number;
+    guild_id: number;
+
+    equipment: CharacterEquipmentRaw[];
+}
--- src/Characters/Characters.ts
+++ src/Characters/Characters.ts
@@ -11,72 +11,160 @@
  */
 
 import {CreateRequest, LimitResponse} from "../Core/Core";
+import {
+	CharacterActiveSpecRaw,
+	CharacterClassRaw,
+	CharacterEquipmentRaw, CharacterEquipmentType,
+	CharacterRaceRaw,
+	CharacterRaw,
+	CharacterRoleRaw
+} from "./CharacterRaw";
+import {Raw} from "../Core/Raw";
 
-export type CharacterEquipmentType = "HEAD" | "NECK" | "SHOULDER" | "CHEST" | "WAIST" | "LEGS" | "FEET"
-	| "WRIST" | "HANDS" | "FINGER_1" | "FINGER_2" | "TRINKET_1" | "TRINKET_2"
-	| "BACK" | "MAIN_HAND" | "OFF_HAND" | "TABARD";
-
-export interface CharacterEquipment {
-	wow_id: number;
-	character_id: number;
-	image_id?: number;
-
-	title: string;
-
-	slot: CharacterEquipmentType;
-	inventory_type: string;
-	level: number;
-
-	quantity: number;
-	quality: number;
-
-	item_class_id: number;
-	item_subclass_id: number;
-
-	stats?: string;
+export enum CharacterSpecType{
+	NONE,
+	TANK,
+	HEALER,
+	MILLIE,
+	RANGE,
 }
 
-export interface CharacterRole {
-	role_index: number;
-	title: string;
+export type CharacterGender = 'FEMALE' | 'MALE';
+
+/**
+ * Character guild rank
+ */
+export class CharacterGuildRank extends Raw<CharacterRoleRaw>{
+	/**
+	 * Returns the title
+	 */
+	public getTitle(): string{
+		return this.getRaw().title
+	}
+
+	/**
+	 * Returns the rank index
+	 */
+	public getIndex(): number{
+		return this.getRaw().role_index;
+	}
 }
 
-export interface CharacterRace {
-	wow_id: number;
-	title: string;
+/**
+ * The character race
+ */
+export class CharacterRace extends Raw<CharacterRaceRaw>{
+	/**
+	 * The character race wow id
+	 */
+	public getWID(){
+		return this.getRaw().wow_id;
+	}
+
+	/**
+	 * The character race title
+	 */
+	public getTitle(): string{
+		return this.getRaw().title;
+	}
 }
 
-export interface CharacterClass {
-	role_index: number;
-	title: string;
+/**
+ * The character race
+ */
+export class CharacterClass extends Raw<CharacterClassRaw>{
+	/**
+	 * The character class wow id
+	 */
+	public getWID(){
+		return this.getRaw().wow_id;
+	}
+
+	/**
+	 * The character class title
+	 */
+	public getTitle(): string{
+		return this.getRaw().title;
+	}
 }
 
-export interface CharacterActiveSpec {
-	wow_id: number;
-	title: string;
-	type: number;
+
+/**
+ * The character spec
+ */
+export class CharacterSpec extends Raw<CharacterActiveSpecRaw>{
+	/**
+	 * The character spec wow id
+	 */
+	public getWID(){
+		return this.getRaw().wow_id;
+	}
+
+	/**
+	 * The character spec title
+	 */
+	public getTitle(): string{
+		return this.getRaw().title;
+	}
+
+	public getType(): CharacterSpecType{
+		return this.getRaw().type;
+	}
 }
 
-export interface Character {
-	wow_id: number;
 
-	name: string;
-	gender: string;
-	level: number;
-	faction: string;
-	gear: number;
+/**
+ * The character
+ */
+export class Character extends Raw<CharacterRaw>{
+	public readonly level: number;
+	public readonly faction: string;
+	public readonly gear: number;
 
-	guild_role: number;
-	meta_text: string;
+	public readonly guild_role: number;
+	public readonly meta_text: string;
 
-	race: CharacterRace;
-	role: CharacterRole;
-	class: CharacterClass;
+	public readonly role: CharacterRoleRaw;
+	public readonly race: CharacterRaceRaw;
+	public readonly class: CharacterClassRaw;
+	public readonly spec: CharacterClassRaw;
 
-	realm_id: number;
-	guild_id: number;
+	public readonly equipment: CharacterEquipmentItem[];
 
-	equipment: CharacterEquipment[];
+	/**
+	 * Returns the wow id
+	 */
+	public getWID(): number{
+		return this.getRaw().wow_id;
+	}
+
+	/**
+	 * Returns the name
+	 */
+	public getName(): string{
+		return this.getRaw().name;
+	}
+
+	/**
+	 * Returns the gender
+	 */
+	public getGender(): CharacterGender{
+		return this.getRaw().gender as CharacterGender;
+	}
+
+	/**
+	 * Returns the level
+	 */
+	public getLevel(): number{
+		return this.getRaw().level;
+	}
+
+	/**
+	 * Returns the character equipment gear
+	 */
+	public getGear(): number{
+		return this.getRaw().gear;
+	}
 }
 
 /**
@@ -84,11 +172,11 @@ export interface Character {
  * @param props
  * @constructor
  */
-export async function GetCharacters(props: { offset?: number, limit?: number } = {}): Promise<LimitResponse<Character>> {
-	const offset = props.offset || 0;
-	const limit = props.limit || 100;
+export async function GetCharacters(props: { offset?: number, limit?: number } = {}): Promise<LimitResponse<CharacterRaw>> {
+    const offset = props.offset || 0;
+    const limit = props.limit || 100;
 
-	return CreateRequest('characters/list', {offset, limit});
+    return CreateRequest('characters/list', {offset, limit});
 }
 
 /**
@@ -97,7 +185,7 @@ export async function GetCharacters(props: { offset?: number, limit?: number } =
  * @param text
  * @constructor
  */
-export async function SetCharacterMetaText(name: string, text: string): Promise<Character> {
-	const __name = name.substr(0, 1).toUpperCase() + name.substr(1).toLocaleLowerCase();
-	return CreateRequest('characters/meta/' + name, {text}, 'PUT');
+export async function SetCharacterMetaText(name: string, text: string): Promise<CharacterRaw> {
+    const __name = name.substr(0, 1).toUpperCase() + name.substr(1).toLocaleLowerCase();
+    return CreateRequest('characters/meta/' + __name, {text}, 'PUT');
 }
