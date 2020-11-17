3fd329c245085a18a24dd760fa651482448a41f4
diego <masnimpu@gmail.com>
2020-11-17 03:48:45 +0300

    added tests

---
+++ tests/characters.test.ts
@@ -0,0 +1,28 @@
+/*
+ * ██╗░░░░░██╗███╗░░██╗░██████╗░░░░██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+ * ██║░░░░░██║████╗░██║██╔════╝░░░░██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+ * ██║░░░░░██║██╔██╗██║██║░░██╗░░░░██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
+ * ██║░░░░░██║██║╚████║██║░░╚██╗░░░██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
+ * ███████╗██║██║░╚███║╚██████╔╝░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
+ * ╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+ *
+ * Developed by Yakov V. Panov (C) Ling • Black 2020
+ * @site http://ling.black
+ */
+
+import {GuildAPI} from "../src";
+
+describe('characters test', () => {
+	test('characters get', async ()=>{
+		const char = await GuildAPI.GetCharacter('дайкирия');
+		console.log(char);
+	});
+	test('mythic list', async () => {
+		const ms = await GuildAPI.GetMythic();
+		const item = ms.response.items[5];
+
+		console.log(item);
+		console.log(item.getCompletedDateTime().getTime())
+		expect(item.getCompletedDateTime().getTime()).toEqual(item.getRaw().completed)
+	});
+});
